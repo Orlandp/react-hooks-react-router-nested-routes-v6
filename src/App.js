afc861e972215c37f779a30aef98ea5f62607862
pknowledge <jamesblssings@gmail.com>
2025-07-20 23:23:25 +0300

    changes onthe src set up the link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar"; // Make sure filename is NavBar.js (capital B)
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      {/* Pass users to all nested routes via Outlet context */}
+      <Outlet context={users} />
     </>
   );
-};
+}
 
 export default App;
+
+
